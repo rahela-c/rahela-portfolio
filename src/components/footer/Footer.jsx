3cdfdb33baab261e92e463686c8c5cd7ab17f6f2
rahela-c <simivicol_ela@yahoo.com>
2023-08-30 21:19:23 +0100

    created the Footer component

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,11 @@
+import { Icon } from "@iconify/react";
+
+import "./Footer.css";
+export default function Footer() {
+  return (
+    <div className="footer">
+      <Icon icon="ic:round-copyright" />
+      <span>2023 | Designed and coded️ by Rahela C</span>
+    </div>
+  );
+}
