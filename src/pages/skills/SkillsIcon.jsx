6267fa401a54cd55c59eabef614d58a738eec4c0
rahela-c <simivicol_ela@yahoo.com>
2023-09-11 23:14:52 +0100

    Refactored Skill icons

--- src/pages/skills/SkillsIcon.jsx
+++ src/pages/skills/SkillsIcon.jsx
@@ -1,7 +1,8 @@
+import { Icon } from "@iconify/react";
 export default function SkillsIcon({ icon, text }) {
   return (
-    <div>
-      <div>{icon}</div>
+    <div className="skillsIcon">
+      <Icon icon={icon} width="64" height="64" />
       <div className="text">{text}</div>
     </div>
   );
--- src/pages/skills/SkillsIcons.jsx
+++ src/pages/skills/SkillsIcons.jsx
@@ -1,73 +1,21 @@
 import SkillsIcon from "./SkillsIcon";
-import { Icon } from "@iconify/react";
 export default function SkillsIcons() {
   return (
     <div className="grid">
       <div className="row1">
-        <SkillsIcon
-          icon={
-            <Icon
-              icon="logos:javascript"
-              color="#10b981"
-              width="64"
-              height="64"
-            />
-          }
-          text="Javascript"
-        />
-        <SkillsIcon
-          icon={
-            <Icon
-              icon="skill-icons:html"
-              color="#10b981"
-              width="64"
-              height="64"
-            />
-          }
-          text="HTML"
-        />
-
-        <SkillsIcon
-          icon={
-            <Icon
-              icon="skill-icons:css"
-              color="#10b981"
-              width="64"
-              height="64"
-            />
-          }
-          text="CSS"
-        />
+        <SkillsIcon text="JavaScript" icon="logos:javascript" />
+        <SkillsIcon text="HTML" icon="skill-icons:html" />
+        <SkillsIcon text="React" icon="logos:react" />
       </div>
+
       <div className="row1">
-        <SkillsIcon
-          icon={
-            <Icon icon="logos:react" color="#10b981" width="64" height="64" />
-          }
-          text="React"
-        />
-
-        <SkillsIcon
-          icon={
-            <Icon icon="logos:jira" color="#10b981" width="64" height="64" />
-          }
-          text="Jira"
-        />
-
-        <SkillsIcon
-          icon={
-            <Icon icon="logos:figma" color="#10b981" width="64" height="64" />
-          }
-          text="Figma"
-        />
+        <SkillsIcon text="Figma" icon="logos:figma" />
+        <SkillsIcon text="Jira" icon="logos:jira" />
+        <SkillsIcon text="CSS" icon="skill-icons:css" />
       </div>
+
       <div className="row1">
-        <SkillsIcon
-          icon={
-            <Icon icon="teenyicons:github-outline" width="64" height="64" />
-          }
-          text="GitHub"
-        />
+        <SkillsIcon text="Github" icon="teenyicons:github-outline" />
       </div>
     </div>
   );
