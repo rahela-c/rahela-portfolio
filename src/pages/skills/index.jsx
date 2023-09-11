9bcbfaf2a0246cea3cebefd47bfbe10cca11b8e9
rahela-c <simivicol_ela@yahoo.com>
2023-09-11 22:43:50 +0100

    Using new Heading component for Skills page

--- src/pages/skills/index.jsx
+++ src/pages/skills/index.jsx
@@ -1,14 +1,14 @@
 import SkillsIcons from "./SkillsIcons";
-import SkillsHeading from "./SkillsHeading";
 import SkillsSubHeading from "./SkillsSubHeading";
 import "./index.css";
+import Heading from "../../components/Heading";
 
 export default function Skills() {
   return (
     <div className="skills">
       <div className="skillsContainers">
-        <SkillsHeading />
-        {/* <SkillsSubHeading /> */}
+        <Heading title="Skills" />
+        <SkillsSubHeading />
         <SkillsIcons />
       </div>
     </div>
