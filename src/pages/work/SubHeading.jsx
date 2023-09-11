3532e23b57714c92782e4984677a50ec1b3651b5
rahela-c <simivicol_ela@yahoo.com>
2023-09-11 22:56:57 +0100

    Using Subheading component for Work

--- src/pages/work/SubHeading.jsx
+++
@@ -1,7 +0,0 @@
-export default function SubHeading() {
-  return (
-    <div className="subHeadingContent">
-      Some of the noteworthy projects I have built:
-    </div>
-  );
-}
--- src/pages/work/index.jsx
+++ src/pages/work/index.jsx
@@ -1,14 +1,14 @@
-import SubHeading from "./SubHeading";
 import Projects from "./Projects";
 import "./index.css";
 import Heading from "../../components/Heading";
+import Subheading from "../../components/Subheading";
 
 export default function Work() {
   return (
     <div className="work">
       <div className="workContainer">
         <Heading title="Work" />
-        <SubHeading />
+        <Subheading text="Some of the noteworthy projects I have built:" />
         <Projects />
       </div>
     </div>
