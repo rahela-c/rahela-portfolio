31a61dd33e6f68f6981fea219a611d1d65cf7d4b
rahela-c <simivicol_ela@yahoo.com>
2023-09-25 13:24:43 +0100

    how to open a new window

--- src/pages/contact/Socials.jsx
+++ src/pages/contact/Socials.jsx
@@ -1,11 +1,19 @@
 import { Icon } from "@iconify/react";
 
 export default function Socials() {
+  function newTab() {
+    window.open("https://github.com/rahela-c", "_blank");
+  }
   return (
     <div className="social">
       <div>You may also find me on these platforms!</div>
       <div className="action">
-        <Icon icon="teenyicons:github-outline" width="24" height="24" />
+        <Icon
+          onClick={newTab}
+          icon="teenyicons:github-outline"
+          width="24"
+          height="24"
+        />
         {/* <Icon icon="lucide:twitter" width="24" height="24" />
         <Icon icon="ph:figma-logo-light" width="24" height="24" /> */}
       </div>
