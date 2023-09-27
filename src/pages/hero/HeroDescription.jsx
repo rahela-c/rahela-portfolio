a280fa798a81c500c19d0652ef4a31a2434add13
rahela-c <simivicol_ela@yahoo.com>
2023-09-27 21:06:38 +0100

    update HeroDescription.jsx and src/pages/hero/index.css

--- src/pages/hero/HeroDescription.jsx
+++ src/pages/hero/HeroDescription.jsx
@@ -4,7 +4,7 @@ export default function HeroDescription() {
   return (
     <div className="content">
       <h1 style={{ textAlign: "center", width: "100%" }}>Hi,I'm Rahela 👋</h1>
-      <div>
+      <div className="content-details">
         As a React Front-End Developer, I aim to contribute to projects by
         writing clean and efficient code. My goal is to continue honing my
         skills to produce user-friendly and maintainable web applications. Open
