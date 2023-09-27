1f04ef43f500c732e24a12d0e6b1ac31863919ee
rahela-c <simivicol_ela@yahoo.com>
2023-09-27 20:22:09 +0100

    update SkillsIcons.jsx and src/pages/skills/index.css

--- src/pages/skills/SkillsIcons.jsx
+++ src/pages/skills/SkillsIcons.jsx
@@ -1,30 +1,15 @@
 import SkillsIcon from "./SkillsIcon";
+
 export default function SkillsIcons() {
   return (
     <div className="grid">
-      <div className="row1">
-        <SkillsIcon text="JavaScript" icon="logos:javascript" />
-      </div>
-      <div className="row1">
-        <SkillsIcon text="HTML" icon="skill-icons:html" />
-      </div>
-      <div className="row1">
-        <SkillsIcon text="React" icon="logos:react" />
-      </div>
-
-      <div className="row1">
-        <SkillsIcon text="Figma" icon="logos:figma" />
-      </div>
-      <div className="row1">
-        <SkillsIcon text="Jira" icon="logos:jira" />
-      </div>
-      <div className="row1">
-        <SkillsIcon text="CSS" icon="skill-icons:css" />
-      </div>
-
-      <div className="row1">
-        <SkillsIcon text="Github" icon="teenyicons:github-outline" />
-      </div>
+      <SkillsIcon text="JavaScript" icon="logos:javascript" />
+      <SkillsIcon text="HTML" icon="skill-icons:html" />
+      <SkillsIcon text="React" icon="logos:react" />
+      <SkillsIcon text="Figma" icon="logos:figma" />
+      <SkillsIcon text="Jira" icon="logos:jira" />
+      <SkillsIcon text="CSS" icon="skill-icons:css" />
+      <SkillsIcon text="Github" icon="teenyicons:github-outline" />
     </div>
   );
 }
