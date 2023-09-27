1621cac889f91f4366032340d15dc4970016d3fc
rahela-c <simivicol_ela@yahoo.com>
2023-09-27 20:53:12 +0100

    build(deps): update package-lock.json, package.json, Header.css and Header.jsx

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,9 +3,13 @@ import Burger from "./Burger";
 import SwitchTheme from "./SwitchTheme";
 import Links from "./Links";
 import "./Header.css";
+import { useEffect } from "react";
+import { useWindowSize } from "@uidotdev/usehooks";
 
 function Header() {
+  const { width } = useWindowSize();
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const isDesktop = width >= 1024;
 
   const openMenu = () => {
     setIsMenuOpen(!isMenuOpen);
@@ -16,13 +20,17 @@ function Header() {
       <div className="container">
         <div className="logo">{"<RC />"}</div>
         <div className="auto"></div>
-        <Burger onClick={openMenu} isMenuOpen={isMenuOpen} />
+        <div></div>
+        {!isDesktop && <Burger onClick={openMenu} isMenuOpen={isMenuOpen} />}
+        {isDesktop && <Links closeMenu={() => setIsMenuOpen(false)} />}
       </div>
 
-      <div className="menu" style={{ display: isMenuOpen ? "flex" : "none" }}>
-        <Links closeMenu={() => setIsMenuOpen(false)} />
-        {/* <SwitchTheme /> */}
-      </div>
+      {!isDesktop && (
+        <div className="menu" style={{ display: isMenuOpen ? "flex" : "none" }}>
+          <Links closeMenu={() => setIsMenuOpen(false)} />
+          {/* <SwitchTheme /> */}
+        </div>
+      )}
     </div>
   );
 }
